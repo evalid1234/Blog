d6033fdfe05c12d517390d9964ff96e60f2080ed
ed lagu <elagu013@fiu.edu>
2023-11-03 00:46:28 -0400

    ui change

---
+++ blog_entries/js/index.js
@@ -0,0 +1,21 @@
+
+const TITLE_TEXT_STATES = ['Java', 'SQL', 'OOP'];
+
+
+const titleAnimation =
+    document.querySelector('.title');
+var titleState = 0;
+
+
+
+// titleAnimation.classList.add('.title');
+
+titleAnimation.addEventListener("animationiteration",
+  () => {
+
+    titleAnimation.innerHTML = `${TITLE_TEXT_STATES[titleState].toUpperCase()}`;
+      titleState = titleState >= TITLE_TEXT_STATES.length - 1 ? 0 : ++titleState;
+      
+    });
+  
+
